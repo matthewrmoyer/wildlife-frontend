1067c02477dbdc2bf97e88b8ee9e550d84730ea8
Matthew Moyer <matthewrmoyer@gmail.com>
2017-05-26 11:13:57 -0600

    put map on page

---
+++ public/app/map/map.controller.js
@@ -0,0 +1,28 @@
+(function() {
+	'use strict'
+	angular.module('app')
+		.component('map', {
+			controller: ('MapController', MapController),
+			templateUrl: './app/map/map.html'
+		})
+
+	MapController.$inject = ['postsService']
+
+	function MapController(postsService) {
+		const vm = this;
+
+		vm.$onInit = function() {
+			console.log('map init')
+		}
+
+		vm.wildlifeMap = L.map('wildlife-map').setView([40.3428, -105.6836], 13);
+
+		vm.wildlifeMapTileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+			maxZoom: 18,
+			id: 'mapbox.satellite',
+			accessToken: 'pk.eyJ1IjoibWF0dGhld3Jtb3llciIsImEiOiJjajM2MzA1YWkwNGZ3MndwNm11NGZuNm1jIn0.Gh0P6Glzi5ERnaHcnwDA3A'
+		}).addTo(vm.wildlifeMap);
+	}
+
+})()
