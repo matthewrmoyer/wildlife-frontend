ba2f4f7852741b71d6a70d58ac7897aa12d64006
Matthew Moyer <matthewrmoyer@gmail.com>
2017-05-29 15:46:00 -0600

    refactor

--- public/app/map/map.controller.js
+++ public/app/map/map.controller.js
@@ -10,38 +10,34 @@
 
 	function MapController(postsService) {
 		const vm = this;
+		// variables
 		vm.posts = postsService.posts;
-		vm.moosePosts = postsService.moosePosts;
-		vm.elkPosts = postsService.elkPosts;
-		vm.bobcatPosts = postsService.bobcatPosts;
+		vm.specieToFilterFor;
+		vm.wildlifeMap;
+		vm.wildlifeMapTileLayer;
+		vm.allMarkerGroup = L.layerGroup() // create a marker group to populate with all posts
+		// functions
 		vm.populateAllMarkerGroup = populateAllMarkerGroup;
 		vm.removeAllMarkers = removeAllMarkers;
-		vm.specieToFilterFor;
-		vm.changeSpecieToFilterFor = changeSpecieToFilterFor;
-
-		// generate map
-		vm.wildlifeMap = L.map('wildlife-map').setView([40.3428, -105.6836], 7);
-		vm.wildlifeMapTileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-			maxZoom: 18,
-			layers: [vm.cities, vm.grayscale],
-			id: 'mapbox.satellite',
-			accessToken: 'pk.eyJ1IjoibWF0dGhld3Jtb3llciIsImEiOiJjajM2MzA1YWkwNGZ3MndwNm11NGZuNm1jIn0.Gh0P6Glzi5ERnaHcnwDA3A'
-		}).addTo(vm.wildlifeMap);
-
-		// create a marker group to populate will all posts
-		vm.allMarkerGroup = L.layerGroup()
-
-		// add allMarkerGroup to map
-		vm.allMarkerGroup.addTo(vm.wildlifeMap) 
+		vm.displayMarkerGroup = displayMarkerGroup;
 
 
 		vm.$onInit = function() {
 			console.log(vm.posts)
+			// generate map
+			vm.wildlifeMap = L.map('wildlife-map').setView([40.3428, -105.6836], 7);
+			vm.wildlifeMapTileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+				attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+				maxZoom: 18,
+				id: 'mapbox.satellite',
+				accessToken: 'pk.eyJ1IjoibWF0dGhld3Jtb3llciIsImEiOiJjajM2MzA1YWkwNGZ3MndwNm11NGZuNm1jIn0.Gh0P6Glzi5ERnaHcnwDA3A'
+			}).addTo(vm.wildlifeMap);
+			// add allMarkerGroup to map
+			displayMarkerGroup(vm.allMarkerGroup, vm.wildlifeMap)
 		}
 
-		// populate allMarkerGroup
-		function populateAllMarkerGroup(post, map){
+		// populate allMarkerGroup array
+		function populateAllMarkerGroup(post, map) {
 			L.marker([post.latitude, post.longitude])
 				.addTo(vm.allMarkerGroup)
 				.bindPopup(`
@@ -53,13 +49,12 @@
 					`);
 		}
 
-		function removeAllMarkers(){
-			vm.allMarkerGroup.clearLayers()
+		function displayMarkerGroup(markerGroup, map) {
+			markerGroup.addTo(map)
 		}
 
-		function changeSpecieToFilterFor(){
-			vm.removeAllMarkers()
-			console.log(vm.specieToFilterFor)
+		function removeAllMarkers(layerGroup) {
+			layerGroup.clearLayers()
 		}
 	}
 
