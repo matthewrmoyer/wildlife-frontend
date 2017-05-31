cb7fa1adbdce0af50e151877c4fb51cf68916a9f
Matthew Moyer <matthewrmoyer@gmail.com>
2017-05-31 10:38:50 -0600

    add ui-router with map route

--- public/app/app.config.js
+++ public/app/app.config.js
@@ -1,12 +1,24 @@
 (function() {
-  'use strict';
+	'use strict';
 
-  angular.module('app').config(config)
+	angular.module('app').config(config)
 
-  config.$inject = []
+	config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider']
 
-  function config(){
+	function config($stateProvider, $urlRouterProvider, $locationProvider) {
+		$locationProvider.html5Mode(true)
 
-  }
+		$stateProvider
+			.state({
+				name: 'home',
+				url: '/',
+				component: 'welcome',
+			})
+			.state({
+				name: 'map',
+				url: '/map',
+				component: 'map',
+			})
+	}
 
 }());
