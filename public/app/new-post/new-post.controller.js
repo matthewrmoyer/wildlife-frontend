a75856284da6f200bfec72f5b487182a6bd28c99
Matthew Moyer <matthewrmoyer@gmail.com>
2017-06-02 14:37:27 -0600

    style new post form

--- public/app/new-post/new-post.controller.js
+++ public/app/new-post/new-post.controller.js
@@ -1,45 +1,60 @@
-(function(){
+(function() {
 	'use strict'
 	angular.module('app')
 		.component('newPost', {
 			controller: ('NewPostController', NewPostController),
 			templateUrl: './app/new-post/new-post.html'
-	})
+		})
+	NewPostController.$inject = ['postsService']
 
-		NewPostController.$inject = ['postsService']
+	function NewPostController(postsService) {
+		const vm = this;
+		vm.specieArray = postsService.specieArray
+		vm.newPost;
+		vm.displayNewSpecieInput = false;
 
-		function NewPostController(postsService) {
-			const vm = this;
-			vm.specieArray = postsService.specieArray
+		vm.userLatitude
+		vm.userLongitude
+		vm.getUserLocation = getUserLocation
+		vm.setUserLocation = setUserLocation
 
-			vm.newPost;
-			vm.displayNewSpecieInput = false;
+		vm.hideNewSpecieInput = hideNewSpecieInput
+		vm.showNewSpecieInput = showNewSpecieInput
+		vm.getSelectedSpecie = getSelectedSpecie
+		vm.$onInit = function() {
+			console.log('new post controller init')
+			vm.getUserLocation()
+		}
 
-			vm.hideNewSpecieInput = hideNewSpecieInput
-			vm.showNewSpecieInput = showNewSpecieInput
-			vm.getSelectedSpecie = getSelectedSpecie
+		function hideNewSpecieInput() {
+			vm.displayNewSpecieInput = false
+		}
 
-			vm.$onInit = function() {
-				console.log('new post controller init')
-			}
+		function showNewSpecieInput() {
+			vm.displayNewSpecieInput = true
+		}
 
-			function hideNewSpecieInput() {
-				vm.displayNewSpecieInput = false
-			}
-
-			function showNewSpecieInput() {
-				vm.displayNewSpecieInput = true
-			}
-
-			function getSelectedSpecie() {
-				if(!vm.newPost.specie){
-					console.log('asdfasdf')
-					vm.showNewSpecieInput()
-				}
-				else{
-					vm.hideNewSpecieInput()
-				}
+		function getSelectedSpecie() {
+			if (!vm.newPost.specie) {
+				console.log('asdfasdf')
+				vm.showNewSpecieInput()
+			} else {
+				vm.hideNewSpecieInput()
 			}
 		}
-	
+
+		function getUserLocation() {
+			if (navigator.geolocation) {
+				navigator.geolocation.getCurrentPosition(setUserLocation);
+			}
+		}
+
+		function setUserLocation(position){
+			vm.userLatitude = position.coords.latitude
+			vm.userLongitude = position.coords.longitude
+			console.log(vm.userLatitude) 
+			console.log(vm.userLongitude)
+		}
+	}
+
 })()
