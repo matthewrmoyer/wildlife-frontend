a57a8ca1b895a6d9fd72c1a111873b67fccd3be0
Matthew Moyer <matthewrmoyer@gmail.com>
2017-06-08 15:22:40 -0600

    ugh

--- public/app/services/auth.service.js
+++ public/app/services/auth.service.js
@@ -19,7 +19,7 @@
       angularAuth0.parseHash(function(err, authResult) {
         if (authResult && authResult.accessToken && authResult.idToken) {
           setSession(authResult);
-          $state.go('map');
+          $state.go('home');
         } else if (err) {
           $timeout(function() {
             $state.go('home');
